68e26b46786f36f10c59a3754ffe411c48217cf2
salvatierra232010 <salvatierra232010@gmail.com>
2025-03-14 11:53:03 -0400

    Actualicion del Java Script

--- app.js
+++ app.js
@@ -1,2 +1,51 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-console.log("hola");
+/*
+Fucionalidades:
+Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en "Adicionar".
+
+Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.
+
+Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
+
+Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la página.
+*/
+
+//Crear un array para lamacenar los nombres
+let amigos = [];
+
+function agregarAmigo(){
+    let amigosNuevos = document.getElementById('amigo').value;
+    if (amigosNuevos === ''){
+        alert ('Por favor, inserte un nombre');
+        return;
+    }
+        amigos.push(amigosNuevos);
+        console.log(amigos);
+        document.getElementById('amigo').value = '';   
+        actualizaLista();
+
+}
+
+function actualizaLista(){
+    let lista = document.getElementById('listaAmigos');
+
+    lista.innerHTML = "";
+    for (let i=0; i < amigos.length; i++){
+        let amigoAgregado = document.createElement('li');
+        amigoAgregado.textContent = amigos[i];
+        lista.appendChild(amigoAgregado);
+    } 
+}
+
+function sortearAmigo(){
+    if (amigos.length === 0){
+        alert("No hay amigos en la lista");
+        return;
+    }
+     let indice = Math.floor(Math.random()*10);
+     let sorteo = amigos[indice];
+     document.getElementById('resultado').innerHTML = `Tu amigo secreto es ${sorteo}`;
+
+}
+
+
