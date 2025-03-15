b0cad59056c71dcf9411e30238e8dbbcacff28a7
salvatierra232010 <salvatierra232010@gmail.com>
2025-03-15 15:58:25 -0400

    Actialidando el READ ME V1

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 
 <h2>Por Fiorella Salvatierra</h2>
 
+
+![mi_Juego](https://github.com/user-attachments/assets/dc3ba10e-513c-49b7-a227-f0036e06cf77)
+
+<li>Descripción de su proyecto:</li>
+<li>funcionalidades:</li>
+<li>Cómo pueden usarlo los usuarios:</li>
+<li>Donde los usuarios pueden encontrar ayuda sobre su proyecto:<li>
+<li>Autores del proyecto:</li>
--- app.js
+++ app.js
@@ -1,55 +1,57 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-/*
-Fucionalidades:
-Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en "Adicionar".
-
-Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.
-
-Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
-
-Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la página.
-*/
-
 //Crear un array para lamacenar los nombres
 let amigos = [];
 
+//Función para agregar amigos
 function agregarAmigo(){
+    //Capturar el valor del campo de entrada
     let amigosNuevos = document.getElementById('amigo').value;
+    //Validar la entrada
     if (amigosNuevos === ''){
         alert ('Por favor, inserte un nombre');
         return;
     }
+    //Actialuzar el array de amigos
         amigos.push(amigosNuevos);
         console.log(amigos);
+    //Limpiar el campo de entrada    
         document.getElementById('amigo').value = '';   
         actualizaLista();
 
 }
-
+//Funcion para actualizar la lista de amigos
 function actualizaLista(){
+    //Obtener el eleento de la lista
     let lista = document.getElementById('listaAmigos');
-
+    //Limpiar la lista existente
     lista.innerHTML = "";
+    //Iterar sobre el arreglo
     for (let i=0; i < amigos.length; i++){
         let amigoAgregado = document.createElement('li');
+        //Agregar elements a la lista
         amigoAgregado.textContent = amigos[i];
         lista.appendChild(amigoAgregado);
     } 
 }
-
+//  Funcion para sortear los amigos
 function sortearAmigo(){
+    //Validar que haya amigos disponibles
     if (amigos.length === 0){
         alert("No hay amigos en la lista");
         return;
     }
+    //Generar un indice aleatorio
      let indice = Math.floor(Math.random()*amigos.length);
+     //Obterner el nombre sorteado
      let sorteo = amigos[indice];
+     //Mostrar el resultado
      document.getElementById('resultado').innerHTML = `Tu amigo secreto es ${sorteo}`;
 
 }
-
+//Funcion para reiniciar el juego
 function limpiarTodo(){
+    //Limpiar la lista de amigos
     amigos = [];
+    //Limpiar el espacio del input, list de amigos y resultado
     document.getElementById('amigo').value = ''
     document.getElementById('listaAmigos').innerHTML = '';
     document.getElementById('resultado').textContent = '';
